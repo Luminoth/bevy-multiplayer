2c88df6b4f95d724f106381f753bbcc276591367
Shane Lillie <ignignokterr@gmail.com>
2024-11-16 23:39:43 -0800

    Poll for placement

--- api/src/handlers/gameclient.rs
+++ api/src/handlers/gameclient.rs
@@ -7,12 +7,33 @@ use axum_extra::TypedHeader;
 use bb8_redis::redis::AsyncCommands;
 use headers::authorization::{Authorization, Bearer};
 use serde::Deserialize;
+use tokio::time::{sleep, timeout, Duration};
 use tracing::{info, warn};
 
 use common::{gameclient::*, user::User};
-use internal::{axum::AppError, notifs::AsNotification};
+use internal::{axum::AppError, notifs::AsNotification, redis::RedisPooledConnection};
 
 use crate::{models, notifs, state::AppState};
+
+const PLACEMENT_TIMEOUT: u64 = 60;
+
+async fn wait_for_placement(
+    conn: &mut RedisPooledConnection,
+    server_id: impl AsRef<str>,
+) -> anyhow::Result<models::gameserver::GameServerInfo> {
+    let key = format!("gameserver:{}", server_id.as_ref());
+    loop {
+        sleep(Duration::from_secs(10)).await;
+
+        let server_info: String = conn.get(&key).await?;
+        let server_info: models::gameserver::GameServerInfo = serde_json::from_str(&server_info)?;
+
+        // TODO: should use a ready flag or something instead?
+        if server_info.state == common::gameserver::GameServerState::InGame {
+            return Ok(server_info);
+        }
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub struct FindServerParamsV1 {}
@@ -27,10 +48,7 @@ pub async fn get_find_server_v1(
 
     info!("finding server for {} ...", user.user_id);
 
-    let mut conn = app_state.redis_connection_pool.get().await?;
-
-    // TODO: this isn't the best way to do this
-    // we have all kinds of race conditions going on here
+    let mut conn = app_state.redis_connection_pool.get_owned().await?;
 
     let server_ids: Vec<(String, u64)> = conn.zpopmin("gameservers:waiting.index", 1).await?;
     if server_ids.len() != 1 {
@@ -43,18 +61,37 @@ pub async fn get_find_server_v1(
 
     let server_info: String = conn.get(format!("gameserver:{}", server_id)).await?;
     let server_info: models::gameserver::GameServerInfo = serde_json::from_str(&server_info)?;
+    if server_info.state != common::gameserver::GameServerState::WaitingForPlacement {
+        // TODO: don't fail, try again until we can't find one
+        warn!("server not waiting for placement!");
+        return Ok(Json(FindServerResponseV1::default()));
+    }
 
     notifs::notify_gameserver(
         &app_state,
-        internal::notifs::PlacementRequestV1::default().as_notification(server_id)?,
-        Some(30),
+        internal::notifs::PlacementRequestV1::default().as_notification(server_id.clone())?,
+        Some(PLACEMENT_TIMEOUT),
     )
     .await?;
 
-    // TODO: wait for the server (with a timeout)
+    // TODO: polling for this kind of sucks,
+    // instead what if we notified clients that are part of the session
+    // when the game server heartbeat happens ?
+
+    let res = timeout(
+        Duration::from_secs(PLACEMENT_TIMEOUT),
+        wait_for_placement(&mut conn, server_id),
+    )
+    .await;
+    if res.is_err() {
+        warn!("placement timeout!");
+        return Ok(Json(FindServerResponseV1::default()));
+    }
+
+    let server_info = res??;
 
     Ok(Json(FindServerResponseV1 {
-        address: "127.0.0.1".to_string(), //server_info.addrs[0].clone(),
+        address: server_info.addrs[0].clone(),
         port: server_info.port,
     }))
 }
